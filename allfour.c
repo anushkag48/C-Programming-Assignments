ae0cdc76ba774ffcce824bef6c7867e0336cea3c
anushkag48 <anushkagoel6454@gmail.com>
2026-08-10 22:41:24 +0530

    Added first two programs

---
+++ allfour.c
@@ -0,0 +1,25 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ calc the Sum, Pro, Diff, Qou*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a=0,b=0,Sum,Pro,Diff;
+    float Qou;
+    printf("enter the first number\n");
+    scanf("%d",&a);
+    printf("enter the second number\n");
+    scanf("%d",&b);
+    Sum = a + b;
+    Pro = a*b;
+    Diff = a-b;
+    Qou = (float)a/b;
+    printf("Sum = %d\n", Sum);
+    printf("Difference = %d\n", Diff);
+    printf("Product = %d\n", Pro);
+    printf("Qoutient: %0.2f\n", Qou);
+    // return 0;
+}
+
---
+++ sum.c
@@ -0,0 +1,18 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ calc the Sum of two numbers*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a=0,b=0,Sum;
+    printf("enter the first number\n");
+    scanf("%d",&a);
+    printf("enter the second number\n");
+    scanf("%d",&b);
+    Sum = a + b;
+    printf("Sum= %d\n", Sum);
+   // return 0;
+}
+
