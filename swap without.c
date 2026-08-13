d3bc0cc0c9f39b01d3bd6018d0ca14aa2a4414d9
anushkag48 <anushkagoel6454@gmail.com>
2026-08-13 11:44:31 +0530

    Day-4

---
+++ swap without.c
@@ -0,0 +1,21 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ calc the SI*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a=0,b=0;
+    printf("enter first number\n");
+    scanf("%d",&a);
+    printf("enter second number\n");
+    scanf("%d",&b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("after swapping:\n");
+    printf("first number = %d\n",a);
+    printf("second number = %d\n",b);
+   // return 0;
+}
