9ad8fd678ce38c4eadbc2685ed2f7eb6a3b5e6cb
anushkag48 <anushkagoel6454@gmail.com>
2026-08-16 20:04:29 +0530

    Day-6

---
+++ evenorodd.c
@@ -0,0 +1,21 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ to find if the no. is even or odd*/
+#include <stdio.h>
+
+int main(void)
+{
+    int n=0;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    if (n % 2 == 0)
+    {
+        printf("The number is even\n");
+}
+    else
+    {
+        printf("The number is odd\n");
+    }
+   // return 0;
+}
---
+++ negorpos.c
@@ -0,0 +1,24 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ to find if the number is negative or zero or positive*/
+#include <stdio.h>
+int main(void)
+{
+    int n=0;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    if (n >0)
+    {
+        printf("The number is positive\n");
+}
+    else if (n<0)
+    {
+        printf("The number is negative\n");
+    }
+    else
+    {
+        printf("The number is zero\n");
+    }
+   // return 0;
+}
