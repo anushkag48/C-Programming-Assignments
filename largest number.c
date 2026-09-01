4af957a90a1a2466e5a35f171dd9a99b7083522c
anushkag48 <anushkagoel6454@gmail.com>
2026-09-01 22:07:33 +0530

    Add files via upload
    Day-8

---
+++ largest number.c
@@ -0,0 +1,25 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ to find if the no. is even or odd*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b,c;
+    printf("enter three numbers\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a>=b && a>=c)
+    {
+        printf(" %d is the largest number\n",a);
+}
+    else if (b>=a && b>=c)
+    {
+       
+            printf("%d is the largest number\n",b);
+    }
+    else{
+        printf("%d is the largest number\n",c);
+    }
+   // return 0;
+}
---
+++ uppercase?.c
@@ -0,0 +1,29 @@
+/* Author: XYZ
+ Date: 06-08-26
+ Description:
+ to find if the input is uppercase, lowercase, digit or speial character*/
+#include <stdio.h>
+
+int main(void)
+{
+   char ch;
+    printf("enter a character\n");
+    scanf("%c",&ch);
+    if (ch>='A' && ch<='Z')
+    {
+        printf("The character is uppercase\n");
+    }
+    else if(ch>='a' && ch<='z')
+    {
+        printf("The character is lowercase\n");
+    }
+else if(ch>='0' && ch<='9')
+{
+    printf("The character is a number\n");
+}
+    else
+    {
+        printf("The character is a special character\n");
+    }
+   // return 0;
+}
